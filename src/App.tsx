76d5e5f45f8673921948ee2770f512d788dd26ee
Hersi Kopani <hersi.kopani@gmail.com>
2025-06-02 12:59:49 +0200

    fix minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,6 @@ import Blogs from './pages/Blogs';
 import BlogDetail from './pages/BlogDetail';
 import Contact from './pages/Contact';
 import NotFound from './pages/NotFound';
-import { initializeApp } from "firebase/app";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCnhMULda_zQR8pBSB9wPSvx4UreaZNDkw",
-  authDomain: "portofolio-b6d8f.firebaseapp.com",
-  projectId: "portofolio-b6d8f",
-  storageBucket: "portofolio-b6d8f.firebasestorage.app",
-  messagingSenderId: "734206212274",
-  appId: "1:734206212274:web:3bfe6ccd7b7e7c4ccdf1ab"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 function App() {
   return (
