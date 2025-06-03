3f47557e63b09f613aa5c2acffabf67afe5171f4
Hersi Kopani <hersi.kopani@gmail.com>
2025-06-03 22:59:57 +0200

    fix path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portofolio/', // Replace with your actual repo name
+  // base: '/portofolio/', // Replace with your actual repo name
   build: {
     outDir: 'dist',
   },
